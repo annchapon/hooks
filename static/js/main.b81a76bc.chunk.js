0c3de6a74e97cc70bc5722333fdab9d055b4bb43
annchapon <annchapon@gmail.com>
2021-09-07 14:04:15 +0300

    Updates

---
+++ static/js/main.b81a76bc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphooks=this.webpackJsonphooks||[]).push([[0],{4:function(e,t,n){"use strict";n.r(t);var o=n(0),r=n.n(o),c=n(3),s=n.n(c),u=n(1),i=r.a.createContext(),j=function(){return Object(u.jsx)(i.Provider,{value:"Hello, World",children:Object(u.jsx)(a,{})})},a=function(){var e=Object(o.useContext)(i);return Object(u.jsx)("p",{children:e})};s.a.render(Object(u.jsx)(j,{}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.b81a76bc.chunk.js.map
